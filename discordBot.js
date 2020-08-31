4c9bb99d811217dc6d33de286869e1be338cd7ae
Thels de Kwant <github@thels.nl>
2020-08-31 12:37:03 +0200

    Reply to Ping messages with Pong.

--- discordBot.js
+++ discordBot.js
@@ -1,8 +1,17 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
+const Discord = require("discord.js")
+const client = new Discord.Client()
 
 // Report successful login:
-client.on("ready", () => console.log("Logged in as:", client.user.tag))
+client.on("ready", () => {
+	console.log("Logged in as:", client.user.tag)
+})
 
-const token = process.env.DISCORD_TOKEN;
-client.login(token);
+// Reply to message:
+client.on("message", msg => {
+	console.log("Received message:", msg)
+	if (msg.content.toLowerCase() === "ping") {
+		msg.reply("Pong")
+	}
+})
+
+client.login(process.env.DISCORD_TOKEN)
