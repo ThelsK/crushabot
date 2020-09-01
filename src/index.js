d9726981c20568e5b655beab98749b183da70881
Thels de Kwant <github@thels.nl>
2020-09-01 09:20:53 +0200

    Move loadDocument code to separate file

--- src/index.js
+++ src/index.js
@@ -1,16 +1,4 @@
 const discordBot = require("./discordBot")
-const { GoogleSpreadsheet } = require('google-spreadsheet')
-async function loadDocument() {
-	console.log("Attempting to Load Document.")
-	const document = new GoogleSpreadsheet(process.env.GOOGLE_DOCUMENT_ID)
-	console.log("Declared Document.")
-	await document.useServiceAccountAuth({
-		client_email: process.env.GOOGLE_CLIENT_EMAIL,
-		private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/gi, "\n")
-	})
-	console.log("Authenticated.")
-	await document.loadInfo()
-	console.log("Loaded Document:", document.title)
-}
+const loadDocument = require("./loadDocument")
 
 loadDocument()
