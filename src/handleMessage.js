99f0e31a431618975ed39478c36538b009a94e0a
Thels de Kwant <github@thels.nl>
2020-09-08 22:34:22 +0200

    Add suppport for Direct Message

--- src/handleMessage.js
+++ src/handleMessage.js
@@ -1,5 +1,8 @@
 const { loadGoogleSheet, getConfig, getCommands, getOutputSheet } = require("./googleSheet")
 const { getUTCDate, formatDate } = require("./date")
+const { config } = require("process")
+
+const lastReminder = {}
 
 async function handleMessage(client, msg) {
 
@@ -8,88 +11,106 @@ async function handleMessage(client, msg) {
 		return
 	}
 
-	// Determine the command and the parameters.
-	const content = msg.content.trim().concat(" ") // Space is added to not confuse commands without parameters.
-	let command = content.substr(0, content.indexOf(" ")).toLowerCase()
-	let parameter = content.substr(content.indexOf(" ") + 1).trim()
-
 	// Refresh the Google Sheets document.
 	let success = await loadGoogleSheet()
 	if (!success) {
 		return
 	}
-
-	//console.log("Client Guilds:", client.guilds)
-
-	// Ignore Private Messages.
-	if (!msg.channel.guild) {
-		msgReply(msg, `This Bot only accepts channel messages, not private messages.`)
-		return
-	}
-
-	// If the Server ID doesn't match the serverid setting, report it.
 	const config = getConfig()
-	if (msg.channel.guild.id !== config.serverid) {
-		if (command.startsWith("!")) {
-			msgError(msg, `Error: Server ID mismatch. I am not supposed to be on this server.`)
+	const commands = getCommands()
+
+	// When receiving a channel message on the wrong server, inform that server's owner.
+	if (msg.guild && msg.guild.id !== config.serverid) {
+		if (!lastReminder[`wrongServer${msg.guild.id}`] ||
+			Date.now() > lastReminder[`wrongServer${msg.guild.id}`] + 600000) {
+			lastReminder[`wrongServer${msg.guild.id}`] = Date.now()
+			msg.guild.owner.send(`Error: Your Server '${msg.guild.name}' with ID '${msg.guild.id}'`
+				+ ` does not match the configured Server ID.`
+				+ `\nEither update the Server ID in the configuration, or remove me from your Server.`
+				+ `\nNote: It can take up to 10 minutes for me to recognize any configuration updates.`)
 		}
 		return
 	}
 
-	// If the channel doesn't match, ignore the message.
-	if (config.inputchannel && msg.channel.name !== config.inputchannel) {
+	// When receiving a channel message on the right server, ignore the incorrect channels.
+	if (msg.guild && config.inputchannel && msg.channel.name !== config.inputchannel) {
 		return
 	}
+
+	// Check if the Bot is connected to the correct server.
+	const guild = client.guilds.cache.find(checkGuild => checkGuild.id === config.serverid)
+	if (!guild) {
+		msgError(msg, com, `Error: I am not connected to the Server with the configured Server ID.`)
+		return
+	}
+
+	// Determine the command and the parameters.
+	const content = msg.content.trim().concat(" ") // Space is added to not confuse commands without parameters.
+	let command = content.substr(0, content.indexOf(" ")).toLowerCase()
+	let parameter = content.substr(content.indexOf(" ") + 1).trim()
 
 	// Check if the command is an alias command.
-	let com = getCommands()[command]
+	let com = commands[command]
 	if (com && com.type === "alias") {
 		if (!com.reference) {
-			msgError(msg, `Error: No reference set for alias command '${command}'.`)
+			msgError(msg, com, `Error: No reference set for alias command '${command}'.`)
 			return
 		}
-		if (!getCommands()[com.reference]) {
-			msgError(msg, `Error: Alias command '${command}' refers to an unknown command '${com.reference}'.`)
+		if (!commands[com.reference]) {
+			msgError(msg, com, `Error: Alias command '${command}' refers to an unknown command '${com.reference}'.`)
 			return
 		}
-		if (getCommands()[com.reference].type === "alias") {
-			msgError(msg, `Error: Alias command '${command}' refers to another alias command '${com.reference}'.`)
+		if (commands[com.reference].type === "alias") {
+			msgError(msg, com, `Error: Alias command '${command}' refers to another alias command '${com.reference}'.`)
 			return
 		}
 		command = com.reference
-		com = getCommands()[command]
+		com = commands[command]
 	}
 
 	// Check if the command exists.
 	if (!com) {
-		if (command.startsWith("!") && getCommands()["!help"]) {
-			msgReply(msg, `Unknown command '${command}'. Type '!help' for an overview of available commands.`)
+		if (command.startsWith("!") && commands["!help"]) {
+			msgReply(msg, com, `Unknown command '${command}'. Type '!help' for an overview of available commands.`)
 		} else if (command.startsWith("!")) {
-			msgReply(msg, `Unknown command '${command}'.`)
+			msgReply(msg, com, `Unknown command '${command}'.`)
 		}
 		return
 	}
 
 	// Check if the command has a valid command type.
 	if (!com.type) {
-		msgError(msg, `Error: No command type set for '${command}'.`)
+		msgError(msg, com, `Error: No command type set for '${command}'.`)
 		return
 	}
 	const types = ["info", "data", "flag", "text", "number", "date"]
 	if (!types.find(type => type === com.type)) {
-		msgError(msg, `Error: Unknown command type '${com.type}' set for '${command}'.`)
+		msgError(msg, com, `Error: Unknown command type '${com.type}' set for '${command}'.`)
+		return
+	}
+
+	// Check if the command is allowed as a channel message.
+	if (msg.guild && com.inchannel !== "TRUE") {
+		msgReply(msg, com, `Command '${command} is not allowed as a channel message. Try it as a direct message.`)
+		return
+	}
+
+	// Check if the command is allowed as a direct message.
+	if (!msg.guild && com.indm !== "TRUE") {
+		msgReply(msg, com, `Command '${command} is not allowed as a direct message. Try it as a channel message.`)
 		return
 	}
 
 	// Check if the user is of adequate rank.
+	const member = await guild.members.fetch(msg.author)
 	if (com.minrank) {
-		let minrank = msg.guild.roles.cache.find(role => role.name === com.minrank)
+		const minrank = guild.roles.cache.find(role => role.name === com.minrank)
 		if (!minrank) {
-			msgError(msg, `Error: Minimum rank '${com.minrank}' for command '${command}' not found.`)
+			msgError(msg, com, `Error: Minimum rank '${com.minrank}' for command '${command}' not found.`)
 			return
 		}
-		if (minrank.rawPosition > msg.member.roles.highest.rawPosition) {
-			msgReply(msg, `Only users with rank '${com.minrank}' or higher can use the '${command}' command.`)
+		if (minrank.rawPosition > member.roles.highest.rawPosition) {
+			msgReply(msg, com, `Only users with rank '${com.minrank}' or higher can use the '${command}' command.`)
 			return
 		}
 	}
@@ -97,9 +118,9 @@ async function handleMessage(client, msg) {
 	// Resolve info type commands.
 	if (com.type === "info") {
 		if (!com.reply) {
-			msgReply(msg, `Error: No reply set for '${command}'.`)
+			msgReply(msg, com, `Error: No reply set for '${command}'.`)
 		} else {
-			msgReply(msg, com.reply)
+			msgReply(msg, com, com.reply)
 		}
 		return
 	}
@@ -107,33 +128,33 @@ async function handleMessage(client, msg) {
 	// Load the output Sheet, and check if the Discord Tag column exists.
 	const outputSheet = getOutputSheet()
 	if (!outputSheet.headerValues.find(value => value === config.discordtagcolumn)) {
-		msgError(msg, `Error: Discord Tag column header '${config.discordtagcolumn}' not found.`)
+		msgError(msg, com, `Error: Discord Tag column header '${config.discordtagcolumn}' not found.`)
 		return
 	}
 	const outputRows = await outputSheet.getRows()
 
 	// Check if there is an entry for the current user.
-	const discordTag = `${msg.author.username}#${msg.author.discriminator}`
+	const discordTag = `${member.user.username}#${member.user.discriminator}`
 	let outputRow = outputRows.find(row => row[config.discordtagcolumn] === discordTag)
 
 	// Resolve stats type commands.
 	if (com.type === "data") {
-		if (!outputRow && getCommands()["!help"]) {
-			msgReply(msg, `No data found for user '${discordTag}'. Type '!help' for an overview of available commands.`)
+		if (!outputRow && commands["!help"]) {
+			msgReply(msg, com, `No data found for user '${discordTag}'. Type '!help' for an overview of available commands.`)
 			return
 		} else if (!outputRow) {
-			msgReply(msg, `No data found for user '${discordTag}'.`)
+			msgReply(msg, com, `No data found for user '${discordTag}'.`)
 			return
 		}
 
 		const outputType = outputRows.find(row => row[config.discordtagcolumn] === "type")
 		if (!outputType) {
-			msgError(msg, `Error: Output row with Discord Tag 'type' not found.`)
+			msgError(msg, com, `Error: Output row with Discord Tag 'type' not found.`)
 			return
 		}
 		const outputDesc = outputRows.find(row => row[config.discordtagcolumn] === "description")
 		if (!outputDesc) {
-			msgError(msg, `Error: Output row with Discord Tag 'description' not found.`)
+			msgError(msg, com, `Error: Output row with Discord Tag 'description' not found.`)
 			return
 		}
 
@@ -153,23 +174,23 @@ async function handleMessage(client, msg) {
 				reply = `${reply}\n${outputDesc[value] || value} ${outputRow[value] || "<blank>"}`
 			}
 		})
-		msgReply(msg, reply)
+		msgReply(msg, com, reply)
 		return
 	}
 
 	// Check if the reference column exists.
 	if (!com.reference) {
-		msgError(msg, `Error: No reference column set for input command '${command}'.`)
+		msgError(msg, com, `Error: No reference column set for input command '${command}'.`)
 		return
 	}
 	if (!outputSheet.headerValues.find(value => value === com.reference)) {
-		msgError(msg, `Error: Reference column header '${com.reference}' for input command '${command}' not found.`)
+		msgError(msg, com, `Error: Reference column header '${com.reference}' for input command '${command}' not found.`)
 		return
 	}
 
 	// Check if a parameter is included.
 	if (!parameter) {
-		msgReply(msg, `Please type '${command} value'.`)
+		msgReply(msg, com, `Please type '${command} value'.`)
 		return
 	}
 
@@ -185,7 +206,7 @@ async function handleMessage(client, msg) {
 		} else if (parameter.startsWith("n") || parameter.startsWith("f") || parameter.startsWith("d") || parameter.startsWith("of")) {
 			parameter = false
 		} else {
-			msgReply(msg, `Please type '${command} ${config.textenabled || "On"}' or '${command} ${config.textdisabled || "Off"}'.`)
+			msgReply(msg, com, `Please type '${command} ${config.textenabled || "On"}' or '${command} ${config.textdisabled || "Off"}'.`)
 			return
 		}
 	}
@@ -197,7 +218,7 @@ async function handleMessage(client, msg) {
 			for (let i in characters) {
 				const character = characters[i]
 				if (parameter.includes(character)) {
-					msgReply(msg, `The text for '${command}' may not include the ' ${character} ' character.`)
+					msgReply(msg, com, `The text for '${command}' may not include the ' ${character} ' character.`)
 					return
 				}
 			}
@@ -205,22 +226,22 @@ async function handleMessage(client, msg) {
 		if (com.minimum) {
 			const minlength = Number(com.minimum)
 			if (isNaN(minlength)) {
-				msgError(msg, `Error: Minimum length '${com.minimum}' for input command '${command}' is not a number.`)
+				msgError(msg, com, `Error: Minimum length '${com.minimum}' for input command '${command}' is not a number.`)
 				return
 			}
 			if (parameter.length < minlength) {
-				msgReply(msg, `The text for '${command}' must be ${minlength} or more characters.`)
+				msgReply(msg, com, `The text for '${command}' must be ${minlength} or more characters.`)
 				return
 			}
 		}
 		if (com.maximum) {
 			const maxlength = Number(com.maximum)
 			if (isNaN(maxlength)) {
-				msgError(msg, `Error: Maximum length '${com.maximum}' for input command '${command}' is not a number.`)
+				msgError(msg, com, `Error: Maximum length '${com.maximum}' for input command '${command}' is not a number.`)
 				return
 			}
 			if (parameter.length > maxlength) {
-				msgReply(msg, `The text for '${command}' must be ${maxlength} or less characters.`)
+				msgReply(msg, com, `The text for '${command}' must be ${maxlength} or less characters.`)
 				return
 			}
 		}
@@ -230,32 +251,32 @@ async function handleMessage(client, msg) {
 	if (com.type === "number") {
 		parameter = Number(parameter)
 		if (com.forbidden && !Number.isInteger(parameter)) {
-			msgReply(msg, `Please type '${command} value'. Value must be a round number.`)
+			msgReply(msg, com, `Please type '${command} value'. Value must be a round number.`)
 			return
 		}
 		if (isNaN(parameter)) {
-			msgReply(msg, `Please type '${command} value'. Value must be a number.`)
+			msgReply(msg, com, `Please type '${command} value'. Value must be a number.`)
 			return
 		}
 		if (com.minimum) {
 			const minvalue = Number(com.minimum)
 			if (isNaN(minvalue)) {
-				msgError(msg, `Error: Minimum value '${com.minimum}' for input command '${command}' is not a number.`)
+				msgError(msg, com, `Error: Minimum value '${com.minimum}' for input command '${command}' is not a number.`)
 				return
 			}
 			if (parameter < minvalue) {
-				msgReply(msg, `The value for '${command}' must be ${minvalue} or more.`)
+				msgReply(msg, com, `The value for '${command}' must be ${minvalue} or more.`)
 				return
 			}
 		}
 		if (com.maximum) {
 			const maxvalue = Number(com.maximum)
 			if (isNaN(maxvalue)) {
-				msgError(msg, `Error: Maximum value '${com.maximum}' for input command '${command}' is not a number.`)
+				msgError(msg, com, `Error: Maximum value '${com.maximum}' for input command '${command}' is not a number.`)
 				return
 			}
 			if (parameter > maxvalue) {
-				msgReply(msg, `The value for '${command}' must be ${maxvalue} or less.`)
+				msgReply(msg, com, `The value for '${command}' must be ${maxvalue} or less.`)
 				return
 			}
 		}
@@ -265,28 +286,28 @@ async function handleMessage(client, msg) {
 	if (com.type === "date") {
 		const date = getUTCDate(parameter)
 		if (isNaN(date)) {
-			msgReply(msg, `Please type '${command} value'. Value must be a valid date.`)
+			msgReply(msg, com, `Please type '${command} value'. Value must be a valid date.`)
 			return
 		}
 		if (com.minimum) {
 			const mindate = getUTCDate(com.minimum)
 			if (isNaN(mindate)) {
-				msgError(msg, `Error: Minimum date '${com.minimum}' for input command '${command}' is not a date.`)
+				msgError(msg, com, `Error: Minimum date '${com.minimum}' for input command '${command}' is not a date.`)
 				return
 			}
 			if (date.getTime() < mindate.getTime()) {
-				msgReply(msg, `The date for '${command}' must be ${formatDate(mindate)} or later.`)
+				msgReply(msg, com, `The date for '${command}' must be ${formatDate(mindate)} or later.`)
 				return
 			}
 		}
 		if (com.maximum) {
 			const maxdate = getUTCDate(com.maximum)
 			if (isNaN(maxdate)) {
-				msgError(msg, `Error: Maximum date '${com.maximum}' for input command '${command}' is not a date.`)
+				msgError(msg, com, `Error: Maximum date '${com.maximum}' for input command '${command}' is not a date.`)
 				return
 			}
 			if (date.getTime() > maxdate.getTime()) {
-				msgReply(msg, `The date for '${command}' must be ${formatDate(maxdate)} or earlier.`)
+				msgReply(msg, com, `The date for '${command}' must be ${formatDate(maxdate)} or earlier.`)
 				return
 			}
 		}
@@ -303,13 +324,13 @@ async function handleMessage(client, msg) {
 
 	// Update the row values.
 	if (config.discordnamecolumn) {
-		outputRow[config.discordnamecolumn] = String(msg.member.nickname || msg.author.username)
+		outputRow[config.discordnamecolumn] = String(member.nickname || member.user.username)
 	}
 	if (config.discordrankcolumn) {
-		outputRow[config.discordrankcolumn] = String(msg.member.roles.highest.name)
+		outputRow[config.discordrankcolumn] = String(member.roles.highest.name)
 	}
 	if (config.rankvaluecolumn) {
-		outputRow[config.rankvaluecolumn] = String(msg.member.roles.highest.rawPosition)
+		outputRow[config.rankvaluecolumn] = String(member.roles.highest.rawPosition)
 	}
 	if (config.lastupdatedcolumn) {
 		outputRow[config.lastupdatedcolumn] = formatDate(new Date(Date.now()))
@@ -329,15 +350,81 @@ async function handleMessage(client, msg) {
 	} else if (parameter === false) {
 		parameter = config.textdisabled || "Off"
 	}
-	msgReply(msg, `${com.reply || `${com.reference} set to`} ${parameter}`)
+	msgReply(msg, com, `${com.reply || `${com.reference} set to`} ${parameter}`)
 }
 
-function msgReply(msg, text) {
-	msg.reply(text)
+async function msgReply(msg, com, text) {
+	console.log(`<= ${msg.author.username}#${msg.author.discriminator}: ${msg.content}`)
+	console.log(`=> ${msg.author.username}#${msg.author.discriminator}: ${text}`)
+	const config = getConfig()
+	const types = ["info", "data", "flag", "text", "number", "date"]
+	let deletemsg = false
+	let replyindm = false
+	if (!com || !types.find(type => com.type === type)) {
+		if (config.deletemsg === "TRUE") {
+			deletemsg = true
+		}
+		if (config.replyindm === "TRUE") {
+			replyindm = true
+			console.log("Should be replying in PM 1")
+		}
+	} else {
+		if (com.deletemsg === "TRUE") {
+			deletemsg = true
+		}
+		if (com.replyindm === "TRUE") {
+			replyindm = true
+			console.log("Should be replying in PM 2")
+		}
+	}
+	if (replyindm) {
+		console.log("Replying in DM.")
+		await msg.author.send(text).catch(error => {
+			reportError(`Error: Unable to send a direct message to '${msg.author.username}#${msg.author.discriminator}'.`)
+			throw error
+		})
+	} else {
+		console.log("Replying as Reply.")
+		await msg.reply(text).catch(error => {
+			reportError(`Error: Unable to send a reply to '${msg.author.username}#${msg.author.discriminator}'.`)
+			throw error
+		})
+	}
+	if (msg.guild && deletemsg) {
+		await msg.delete().catch(error => {
+			reportError(`Error: Unable to delete the message from '${msg.author.username}#${msg.author.discriminator}'.`)
+			throw error
+		})
+	}
 }
 
-function msgError(msg, text) {
-	msg.reply(text)
+async function msgError(msg, com, text) {
+	const config = getConfig()
+	if (!config.ownertag) {
+		msgReply(msg, com, text)
+		return
+	}
+	console.log(`<= ${msg.author.username}#${msg.author.discriminator}: ${msg.content}`)
+	console.log(`=> ${config.ownertag}: ${text}`)
+	await client.users.cache.get(config.ownertag).send(text).catch(error => {
+		reportError(`Error: Unable to send direct message to '${config.ownertag}'.`)
+		throw error
+	})
+	if (!com || !types.find(type => com.type === type)) {
+		if (config.deletemsg === "TRUE") {
+			deletemsg = true
+		}
+	} else {
+		if (com.deletemsg === "TRUE") {
+			deletemsg = true
+		}
+	}
+	if (msg.guild && deletemsg) {
+		await msg.delete().catch(error => {
+			reportError(`Error: Unable to delete the message from '${msg.author.username}#${msg.author.discriminator}'.`)
+			throw error
+		})
+	}
 }
 
 module.exports = { handleMessage }
