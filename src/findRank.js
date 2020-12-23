2944d3c51487b8648a58dced0cf5051f5f816648
Thels de Kwant <github@thels.nl>
2020-12-23 14:35:06 +0100

    Move Rank Found logic to the end of findRank

--- src/findRank.js
+++ src/findRank.js
@@ -23,20 +23,20 @@ function findRank(member) {
 
 	// Find the user rank that is listed highest in the list of ranks.
 	rankData = ranks.find(rank => member.roles.cache.find(role => rank.rankid === role.id))
-	if (rankData) {
-		return rankData
-	}
 
 	// The user doesn't have a rank that is listed in the list of ranks.
-	rankData = ranks.find(rankEntry => rankEntry.rankid === config.allothersrankid)
 	if (!rankData) {
-		reportError(`Error: Cannot find rank '${config.allothersrankid}'.`)
-		rankData = {
-			rankid: config.allothersrankid,
-			rank: "Rank not found",
-			weight: 0,
-			command: 0,
+		rankData = ranks.find(rankEntry => rankEntry.rankid === config.allothersrankid)
+		if (!rankData) {
+			reportError(`Error: Cannot find rank '${config.allothersrankid}'.`)
+			rankData = {
+				rankid: config.allothersrankid,
+				rank: "Rank not found",
+				weight: 0,
+				command: 0,
+			}
 		}
+		return rankData
 	}
 	return rankData
 }
